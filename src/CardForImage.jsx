a48a1ab1cc797ce8791ae972fa165a1461f8e72b
Antriksh rawat <antrikshrawat2@gmail.com>
2025-04-15 23:42:42 +0530

    ain

--- src/CardForImage.jsx
+++ src/CardForImage.jsx
@@ -31,16 +31,16 @@ export default function CardForImage({ imageUrl, hdimageUrl,mediaType, title, de
     <div className="p-4 w-full mt-5">
       <div className="absolute right-6 -mt-3 bg-[#72383d] text-[#efe9e1] w-28 rounded-sm text-center font-bold">{date}</div>
       <div className="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
-{isImage?<img
+{isImage?(<img
           className="md:h-lvh w-full object-cover object-center transition-opacity duration-500 opacity-100 aspect-video"
           src={url}
           alt="blog"
-        />:
+        />):(mediaType!=="other"?
         <iframe
   className="md:h-lvh w-full object-cover object-center transition-opacity duration-500 opacity-100 aspect-video"
   src={imageUrl}
   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-/>}
+/>:<div className="md:h-lvh my-9 w-full flex justify-center items-center md:text-5xl text-3xl">No Preview Available</div>)}
         <div className="p-6">
           <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1"></h2>
           <h1 className="title-font text-lg font-medium text-white mb-3">{title}</h1>
